1b138f66ad7d46c1516a7fe67d375fa9feeda69e
Mario <mariobvdaw13@gmail.com>
2023-10-31 12:21:38 +0000

    Formulario-Imagen

--- tema3/practica8/formulario.php
+++ tema3/practica8/formulario.php
@@ -20,11 +20,30 @@
     <?php
         $errores = array();
          if (enviado() && validaFormulario($errores)) {
-            echo "bien";
+            foreach ($_REQUEST as $key => $value) {
+                if ($key=="archivo") {
+                    
+                    $ruta = "/var/www/servidor/tema3/practica8/";
+                    $ruta .= basename($_FILES['archivo']['name']);
+                    if(move_uploaded_file($_FILES['archivo']['tmp_name'], $ruta)){
+                        echo 'Imagen: <img src="./'.$_FILES['archivo']['name'].'" alt="imagen">';
+                    }
+                    
+                } else{
+                    if ($key=="checks") {
+                        echo "Checks:";
+                        for ($i=0; $i <count($value) ; $i++) { 
+                            echo "<p> - " . $value[$i] . "</p>";
+                        }
+                    } else{
+                        echo "<p>" . $key . ": " . $value . "</p>";
+                    }
+                }
+            }
         } else {
     ?>
     <h1>Formulario de registro</h1>
-    <form action="" method="post" enctype="multipart/form-data">
+    <form action="" method="get" enctype="multipart/form-data">
         <label for="alfa1">Alfabetico
             <input type="text" name="alf1" id="alf1" placeholder="Nombre"
             value=<?php recuerda("alf1")?>>
@@ -34,78 +53,101 @@
         <label for="alfa2">Alfabetico opcional
             <input type="text" name="alf2" id="alf2" placeholder="Nombre"
             value=<?php recuerda("alf2")?>>
+            <?php errores($errores,"alf2");?>
         </label>
         <br><br>
+
         <label for="alfnum1">Alfanumerico
             <input type="text" name="alfnum1" id="alfnum1" placeholder="Apellido"
             value=<?php recuerda("alfnum1")?>>
             <?php errores($errores,"alfnum1");?>
         </label>
         <br><br>
+
         <label for="alfnum2">Alfanumerico opcional
             <input type="text" name="alfnum2" id="alfnum2" placeholder="apellido"
             value=<?php recuerda("alfnum2")?>>
+            <?php errores($errores,"alfnum2");?>
         </label>
         <br><br>
+
         <label for="num1">Numerico
             <input type="number" name="num1" id="num1" placeholder="12"
             value=<?php recuerda("num1")?>>
             <?php errores($errores,"num1");?>
         </label>
         <br><br>
+
         <label for="num2">Numerico opcional
             <input type="number" name="num2" id="num2" placeholder="32"
             value=<?php recuerda("num2")?>>
         </label>
         <br><br>
+
         <label for="fecha1">Fecha
             <input type="date" name="fecha1" id="fecha1"
             value=<?php recuerda("fecha1")?>>
             <?php errores($errores,"fecha1");?>
         </label>
         <br><br>
+
         <label for="fecha2">Fecha opcional
             <input type="date" name="fecha2" id="fecha2"
             value=<?php recuerda("fecha2")?>>
         </label>
         <br><br>
+
         <label for="opcion1">Opcion1
-            <input type="radio" name="opcion" id="opcion1" value="opcion1">
+            <input type="radio" name="opcion" id="opcion1" value="opcion1" 
+            <?php recuerdaRadio("opcion", "opcion1");?>
+            >
         </label>
         <label for="opcion2">Opcion2
-            <input type="radio" name="opcion" id="opcion2" value="opcion2">
+            <input type="radio" name="opcion" id="opcion2" value="opcion2"
+            <?php recuerdaRadio("opcion", "opcion2");?>
+            >
         </label>
         <label for="opcion3">Opcion3
-            <input type="radio" name="opcion" id="opcion3" value="opcion3">
+            <input type="radio" name="opcion" id="opcion3" value="opcion3"
+            <?php recuerdaRadio("opcion", "opcion3");?>
+            >
         </label>
         <br><br>
-        <label for="select">Select
-            <select name="select" id="">
-                <option value="0">- select1 -</option>
-                <option value="opcion1">opcion1</option>
-                <option value="opcion2">opcion2</option>
-                <option value="opcion3">opcion3</option>
-            </select>
-        </label>
+        
+        <p>Select</p>
+        <select name="combo" id="">
+            <option value="0">- select1 -</option>
+            <option <?php recuerdaSelect("combo" , "opcion1") ?> value="opcion1">opcion1</option>
+            <option <?php recuerdaSelect("combo" , "opcion2") ?> value="opcion2">opcion2</option>
+            <option <?php recuerdaSelect("combo" , "opcion3") ?> value="opcion3">opcion3</option>
+        </select>
+        <?php errores($errores,"combo");?>
+
         <p>check</p>
         <?php
             for ($i=1; $i < 7; $i++) { 
                 echo '<label for="ch' . $i . '">';
-                echo '<input type="checkbox" name="checks[]" id="ch' . $i . '" value="ch' . $i .'">' . "check" . $i . '<br>';
+                echo '<input type="checkbox" name="checks[]" id="ch' . $i . '" 
+                value="ch' . $i .'"'. recuerdaCheck("checks","ch".$i) .'>' . "check" . $i . '<br>';
                 echo '</label>';
             }
         ?>
+        <?php errores($errores,"checks");?>
         <br><br>
         <label for="telefono">Telefono
-            <input type="text" name="telefono" id="telefono" placeholder="123456789">
+            <input type="text" name="telefono" id="telefono" placeholder="123456789"
+            value=<?php recuerda("telefono")?>>
         </label>
         <br><br>
         <label for="correo">Email
-            <input type="email" name="correo" id="correo" placeholder="prueba@prueba.com">
+            <input type="email" name="correo" id="correo" placeholder="prueba@prueba.com"
+            value=<?php recuerda("correo")?>>
+            <?php errores($errores,"correo");?>
         </label>
         <br><br>
         <label for="pass">Contraseña
-            <input type="password" name="pass" id="pass">
+            <input type="password" name="pass" id="pass" value=<?php recuerda("pass")?>>
+            <?php errores($errores,"pass");?>
         </label>
         <br><br>
         <label for="archivo">Subir documento
@@ -117,6 +159,7 @@
     </form>
     <?php
         }
+        
     ?>
 </body>
 </html>
--- tema3/practica8/validaciones.php
+++ tema3/practica8/validaciones.php
@@ -12,25 +12,132 @@
         }
         return false;
     }
+    function alfalbetico($name){
+        if(ctype_alpha($name)){
+            return false;
+        }
+        return true;
+    }
+    function alfanumerico($name){
+        if(ctype_alnum($name)){
+            return false;
+        }
+        return true;
+    }
+    function numerico($name){
+        if(is_numeric($name)){
+            return false;
+        }
+        return true;
+    }
     function recuerda($name){
         if (enviado() && !empty($_REQUEST[$name])) {
             echo $_REQUEST[$name];
         }
     }
+    function numeroMinMax($numero){
+        if($numero>0 && $numero<101){
+            return false;
+        }
+        return true;
+    }
+
+    function mmenorEdad($fecha){
+        $hoy = new DateTime();
+        $intervalo = $fecha->diff($hoy);
+        if ($intervalo['y']>=18) {
+            return false;
+        }
+        return true;
+    }
+    function recuerdaRadio($name, $value){
+        if (enviado() && isset($_REQUEST[$name]) && $_REQUEST[$name]==$value) {
+            echo "checked";
+        } 
+    }
+
+    function selectValido($select){
+        if ($select == 0){
+            return true;
+        }
+        return false;
+    }
+    function recuerdaSelect($name, $value){
+        if (enviado() && isset($_REQUEST[$name]) && $_REQUEST[$name]==$value) {
+            echo "selected";
+        } 
+    }
+    function recuerdaCheck($name, $value){
+        if (enviado() && isset($_REQUEST[$name]) && in_array($value, $_REQUEST[$name])) {
+            return "checked";
+        } 
+    }
+    function checkMinMax($check){
+        if(enviado() && count($check)>=1 && count($check)<4){
+            return false;
+        }
+        return true;
+    }
+    // function emailValido($email){
+    //     return filter_var($email, FILTER_VALIDATE_EMAIL);
+    // }
+
     function validaFormulario(&$errores){
         $vacio="Campo vacio";
         if (textoVacio($_REQUEST['alf1'])) {
-            $errores['alf1']=$vacio;
+            $errores['alf1'] = $vacio;
+        } else{
+            if (alfalbetico($_REQUEST['alf1'])){
+                $errores['alf1'] = "Debe ser alfabetico";
+            }
+        }
+        if (!textoVacio($_REQUEST['alf2']) && alfalbetico($_REQUEST['alf2'])){
+            $errores['alf2'] = "Debe ser alfabetico";
         }
         if (textoVacio($_REQUEST['alfnum1'])) {
-            $errores['alfnum1']=$vacio;
+            $errores['alfnum1'] = $vacio;
+        }else{
+            if (alfanumerico($_REQUEST['alfnum1'])){
+                $errores['alfnum1'] = "Debe ser alfanumerico";
+            }
+        }
+        if (!textoVacio($_REQUEST['alfnum2']) && alfanumerico($_REQUEST['alfnum2'])){
+            $errores['alfnum2'] = "Debe ser alfanumerico";
         }
         if (textoVacio($_REQUEST['num1'])) {
-            $errores['num1']=$vacio;
+            $errores['num1'] = $vacio;
+        } else{
+            if (numerico($_REQUEST['num1'])) {
+                $errores['num1'] = "Debe ser numerico";
+            } else {
+                if (numeroMinMax($_REQUEST['num1'])){
+                    $errores['num1'] = "El numero debe estar entre 1 y 100";
+                }
+            }
+        if (numerico($_REQUEST['num2'])) {
+            $errores['num2'] = "Debe ser numerico";
+        }  
         }
         if (textoVacio($_REQUEST['fecha1'])) {
             $errores['fecha1']=$vacio;
+        } else{
+            if (menorEdad($_REQUEST['fecha1'])){
+                $errores['fecha1']="Es menor de edad";
+            }
         }
+        if (selectValido($_REQUEST['combo'])) {
+            $errores['combo'] = "Seleccina una opción";
+        }
+        // if (checkMinMax($_REQUEST['checks'])){
+        //     $errores['checks'] = "Debes seleccionar entre 1 y 3 checks";
+        // }
+        if (textoVacio($_REQUEST['correo'])) {
+            $errores['correo']=$vacio;
+        }
+        if (textoVacio($_REQUEST['pass'])) {
+            $errores['pass']=$vacio;
+        }
+        
         if (count($errores)==0) {
             return true;
         }
